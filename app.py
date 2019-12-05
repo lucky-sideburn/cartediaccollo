063d1e657759b2e9b773c09163f82a8f2f79add4
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2019-12-05 22:40:21 +0100

    added accolli dashboard

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from PIL import ImageFont, Image, ImageDraw
-from flask import Flask, render_template, request, send_file, redirect
+from flask import Flask, render_template, request, send_file, redirect, url_for
 from datetime import datetime
 import time
 import qrcode
@@ -12,13 +12,64 @@ import string
 import secrets
 import random
 import smtplib
+import base64
 
 app = Flask(__name__)
+client = MongoClient()
+client = MongoClient('localhost', 27017)
 
 def randomString(stringLength=10):
-    """Generate a random string of fixed length """
     lettersAndDigits = string.ascii_lowercase + string.digits
     return ''.join(random.choice(lettersAndDigits) for i in range(stringLength))
+
+
+# return an array of accolli related to a sepicific dashboard
+def find_accolli_for_dashboard(dashboard_id):
+  accolli = []
+  db = client['cartediaccollo']
+  collection = db['carte']
+  cards = collection.find({"dashboard_id": dashboard_id}) 
+  for doc in cards:
+    accolli.append(doc)
+  return accolli
+
+# check if the dashboard exists and return a dashboard hash
+def is_valid_dashboard(dashboard_id):
+  db = client['cartediaccollo']
+  collection = db['dashboards']
+  dashboard = collection.find_one({"uuid": dashboard_id})
+  if dashboard != None:
+    return dashboard 
+  else:
+    return None
+
+def check_valid_authorization(dashboard_id,token):
+  db = client['cartediaccollo']
+  collection = db['dashboards']
+  dashboard = collection.find_one({"uuid": dashboard_id, "token": token})
+  if dashboard != None:
+    print('Dashboard' + dashboard_id + " with token " + token + " found!")
+    return dashboard 
+  else:
+    print('Dashboard' + dashboard_id + " with token " + token + " not found!")
+    return None
+
+def generate_dashboard_id_and_token(name):
+  db = client['cartediaccollo']
+  collection = db['dashboards']
+  dashboard_id = str(uuid.uuid4())
+  while True:
+    card = collection.find_one({"uuid": dashboard_id})
+    if card == None:
+      break
+    else:
+      dashboard_id = str(uuid.uuid1())
+  dashboard = {"uuid": dashboard_id, "token": randomString(20), "name": name}
+  print("Try to write a dasboard to MongoDB " + str(dashboard))
+  if collection.insert_one(dashboard).inserted_id != None:
+    return dashboard
+  else:
+    False
 
 def read_card_mongo(uuid,token):
   client = MongoClient()
@@ -32,12 +83,12 @@ def read_card_mongo(uuid,token):
   else:
     return card
 
-def write_card_mongo(card_id,token,card_url,sender):
+def write_card_mongo(card_id,token,card_url,sender,dashboard_id):
   client = MongoClient()
   client = MongoClient('localhost', 27017)
   db = client['cartediaccollo']
   collection = db['carte']
-  post = {"uuid": card_id, "token": token, "status": "open", "url": card_url, "sender": sender}
+  post = {"uuid": card_id, "token": token, "status": "open", "url": card_url, "sender": sender, "dashboard_id": dashboard_id}
   post_id = collection.insert_one(post).inserted_id
   print post_id
 
@@ -50,9 +101,7 @@ def change_card_status(card_id,new_status):
   newvalues = { "$set": { "status": new_status } }
   return collection.update_one(myquery, newvalues)
 
-
-def create_card_img(recipient,task,sender,token):
-  card_id = str(uuid.uuid1())
+def create_card_img(card_id,recipient,task,sender,token):
   img = Image.new('RGB', (1024, 800), color = (0, 0, 0))
   d = ImageDraw.Draw(img)
   
@@ -104,11 +153,71 @@ def create_card_img(recipient,task,sender,token):
 
 @app.route("/")
 def main():
-    return render_template('index.html', accolloform=False, slideshow=True)
+  if request.args.get('id') != None:
+    dashboard = is_valid_dashboard(request.args.get('id'))
+    if dashboard != None:
+      return render_template('index.html',accolloformdashboard=True, accolloform=False, dashboard=dashboard)
+  else:
+    return render_template('index.html', accolloform=True)
 
 @app.route("/create")
 def create():
-    return render_template('index.html', accolloform=True, slideshow=False)
+  return render_template('index.html', accolloform=True)
+
+@app.route("/delete")
+def delete():
+  client = MongoClient()
+  client = MongoClient('localhost', 27017)
+  db = client['cartediaccollo']
+  collection = db['carte']
+  data = base64.b64decode(request.args.get('token'))
+  data_split = data.split(':')
+  dashboard_id = data_split[0]
+  token = data_split[1]  
+  myquery = { "uuid": request.args.get('id'), "dashboard_id": dashboard_id }
+  print "Deleted card: " + str(collection.delete_one(myquery))
+  return redirect('/dashboard_accolli?token=' + request.args.get('token'), code=302)
+
+@app.route("/dashboard_accolli")
+def dashboard_accolli():
+  
+  authenticated_dashboard = None
+  accolli = {}
+
+  if 'Authorization' in request.headers:
+    print("client has Header Authorization: " + request.args.get('token'))
+    authorization_header = request.headers['Authorization']
+    data = base64.b64decode(authorization_header)
+    dashboard_split_array = data.split(':')
+    dashboard_id = dashboard_split_array[0]
+    token = dashboard_split_array[1]
+    authenticated_dashboard = check_valid_authorization(dashboard_id,token)
+
+  elif request.args.get('token') != None:
+    print("client has token: " + request.args.get('token'))
+    data = base64.b64decode(request.args.get('token'))
+    print data
+    dashboard_split_array = data.split(':')
+    dashboard_id = dashboard_split_array[0]
+    token = dashboard_split_array[1]
+    print('dashboard id: ' + dashboard_id + ' - token: ' +  token)
+    authenticated_dashboard = check_valid_authorization(dashboard_id,token)
+
+  if authenticated_dashboard != None:
+    print "User authenticated whith uuid " + authenticated_dashboard['uuid']
+    accolli = find_accolli_for_dashboard(authenticated_dashboard['uuid'])
+    return render_template('dashboard_accolli.html', token=request.args.get('token'), alert=False, id=dashboard_id, dashboard_name=authenticated_dashboard['name'], accolli=accolli) 
+  else:
+    return render_template('dashboard_accolli.html', view_form=True, dashboard_name='', accolli = accolli)
+
+@app.route("/dashboard_accolli/create", methods = ['POST', 'GET'])
+def create_dashboard_accolli():
+  result = request.form
+  if ((len(result['nome_dashboard']) < 1) or (len(result['nome_dashboard']) > 30)):
+    return redirect(url_for('dashboard_accolli', input_alert=1))
+
+  dashboard = generate_dashboard_id_and_token(result['nome_dashboard'])
+  return redirect('/dashboard_accolli?token=' + base64.b64encode(dashboard['uuid'] + ':' + dashboard['token']), code=302)
 
 @app.route("/message",methods = ['POST', 'GET'])
 def message():
@@ -174,22 +283,35 @@ def show():
 @app.route('/cartadiaccollo',methods = ['POST', 'GET'])
 def cartadiaccollo():
   if request.method == 'POST':
-    result = request.form
+    if request.args.get('to') == 'dashboard':
+      result = request.form
+      inputs = ["sender","task"]
+      for _input in inputs:
+        if ((len(result[_input]) < 1) or (len(result[_input]) > 60)):
+          return render_template('index.html', accolloformdashboard=True, alert=True, accolloform=True)
+      
+      token = randomString(20)
+      print("Foo " + request.args.get('dashboard_id'))
+      card_id = create_card_img(str(uuid.uuid1()),request.args.get('dashboard_name'),result['task'],result['sender'],token)
+      card_url = "https://accolli.it/show?id=" + card_id + "&token=" + token 
+      write_card_mongo(card_id,token,card_url,result['sender'],request.args.get('dashboard_id'))
+      return redirect(card_url, code=302)
 
-    inputs = ["recipient","sender","task"]
+    else:
+      result = request.form
+      inputs = ["recipient","sender","task"]
+      for _input in inputs:
+        if ((len(result[_input]) < 1) or (len(result[_input]) > 60)):
+          return render_template('index.html', alert=True, accolloform=True)
 
-    for _input in inputs:
-      if ((len(result[_input]) < 1) or (len(result[_input]) > 60)):
-        return render_template('index.html', alert=True, accolloform=True, slideshow=False)
-
-    token = randomString(20)
-    card_id = create_card_img(result['recipient'],result['task'],result['sender'],token)
-    card_url = "https://accolli.it/show?id=" + card_id + "&token=" + token 
-    print card_url
-    write_card_mongo(card_id,token,card_url,result['sender'])
-    #return send_file('static/cards/' + card_id + '.png', mimetype='image/png', attachment_filename='CartaDiAccollo.png')
-    card_url = "https://accolli.it/show?id=" + card_id + "&token=" + token
-    return redirect(card_url, code=302)
+      token = randomString(20)
+      card_id = create_card_img(str(uuid.uuid1()),result['recipient'],result['task'],result['sender'],token)
+      card_url = "https://accolli.it/show?id=" + card_id + "&token=" + token 
+      print card_url
+      write_card_mongo(card_id,token,card_url,result['sender'],None)
+      #return send_file('static/cards/' + card_id + '.png', mimetype='image/png', attachment_filename='CartaDiAccollo.png')
+      card_url = "https://accolli.it/show?id=" + card_id + "&token=" + token
+      return redirect(card_url, code=302)
   else:
     return redirect('https://accolli.it',code=302) 
 
