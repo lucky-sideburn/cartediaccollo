db4fe86c599b222603876e5f4e7092e74f022257
Eugenio Marzo <eugenio.marzo@sourcesense.com>
2019-12-29 20:00:13 +0100

    disabling caching

--- app.py
+++ app.py
@@ -371,6 +371,18 @@ def register():
 
     return render_template('register.html')
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
